db1b879bb1b913d95589ca39919dc4c192fe5bcd
pdkproitf <phucdaica.pro@gMAIL.com>
2016-07-30 16:16:25 +0700

    a

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   resources :publishes
   resources :categories
+  resources :orders
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
